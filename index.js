43fe6c29f40e3e7683bf0f20a9105a76a4e37a0a
fugroup <vidar@fugroup.net>
2018-10-20 16:55:12 +0200

    Update

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const itility = {}
 
 itility.mode = (x, d, s, p) => {
   if (typeof p === 'undefined') { p = s; s = undefined }
-  const a = { 'development': d, 'staging': s, 'production': p }
+
+  if (typeof x !== 'string') {
+    throw new Error(`The first parameter must be a string`)
+  }
+
+  let a
+  if (typeof x === 'object') {
+    a = x
+  } else  {
+    a = { 'test': d, 'development': d, 'staging': s, 'production': p }
+  }
+
   const m = process.env.NODE_ENV || 'development'
+
   if (!a[m]) {
     throw new Error(`Mode ${m} not found`)
   }
+
   return x.replace(/\*/, a[m])
 }
 
